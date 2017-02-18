26da29f1a140df70d16103c6a3ecfe34c228307d
Binh Nguyen <b.nguyen.sn@gmail.com>
2017-02-18 22:57:12 +0000

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = (env) => {
+    const isProduction = env.production === true;
+    const platflorm = env.platform;
+
+    return {
+        entry: {
+
+        },
+        output: {
+
+        },
+        module: {
+
+        },
+        plugins: {
+
+        },
+        devServer: {
+
+        }
+    }
+};
